c9cc2b623748e0224bcfbac4b4c466b59eb623d6
Kornienko Denis <korney197823@gmail.com>
2016-02-14 16:17:54 +0300

    add js and animation form

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,7 @@
 var link = document.querySelector(".link");
 var popup = document.querySelector(".modal-content");
 
-//link.onclick = function() {
-//  event.preventDefault();
-//  if (popup.style.display === 'none') {
-//    popup.style.display = 'block';
-//  }
-//  else {
-//    popup.style.display = 'none';
-//  }
-//};
-
 link.addEventListener("click", function(event) {
   event.preventDefault();
-    if (popup.style.display === 'none') {
-    popup.style.display = 'block';
-  }
-   else {
-      popup.style.display = 'none';
-    }
+  popup.classList.toggle("modal-content-show");
 });
-
-//link.addEventListener("click", function(event) {
-//  event.preventDefault();
-//  popup.classList.remove("modal-content-show");
-//});
