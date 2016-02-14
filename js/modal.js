0afb04057b8c7b406bfb34d9a8e6e044e01e3f63
Kornienko Denis <korney197823@gmail.com>
2016-02-14 15:30:08 +0300

    refact form and add decorative elements

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,27 @@
 var link = document.querySelector(".link");
 var popup = document.querySelector(".modal-content");
 
+//link.onclick = function() {
+//  event.preventDefault();
+//  if (popup.style.display === 'none') {
+//    popup.style.display = 'block';
+//  }
+//  else {
+//    popup.style.display = 'none';
+//  }
+//};
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("modal-content-show");
+    if (popup.style.display === 'none') {
+    popup.style.display = 'block';
+  }
+   else {
+      popup.style.display = 'none';
+    }
 });
 
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.remove("modal-content-show");
-});
+//link.addEventListener("click", function(event) {
+//  event.preventDefault();
+//  popup.classList.remove("modal-content-show");
+//});
