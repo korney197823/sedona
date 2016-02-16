4be4aa25e7f1a98a9df794709287c5869458da2f
Kornienko Denis <korney197823@gmail.com>
2016-02-16 23:05:05 +0300

    Make validation form

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,19 @@
-var link = document.querySelector(".link");
-var popup = document.querySelector(".modal-content");
+var link = document.querySelector(".link"),
+    popup = document.querySelector(".modal-content"),
+    dateIn = popup.querySelector("[name=checkin]"),
+    dateOut = popup.querySelector("[name=checkout]"),
+    form = popup.querySelector("form");
+
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("modal-content-show");
+  dateIn.focus();
 });
+
+form.addEventListener("submit", function(event) {
+  if (!dateIn.value || !dateOut.value)
+      event.preventDefault();
+      console.log("Введите даты заезда и выезда");
+});
+
